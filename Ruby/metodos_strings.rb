7725d8bd0b926548b30c08d25e2191cec3130eb6
Thomas Jefferson <thjefferson06@gmail.com>
2024-07-19 18:09:01 -0300

    Método (lenght) com a função  de contar os caracteres

---
+++ Ruby/metodos_strings.rb
@@ -0,0 +1,6 @@
+#métodos para saber quantos caracteres tem uma string pode ou nao usar ()
+mensagem = "ooo"
+puts mensagem.length()
+
+#Posso tambem juntar as execucoes de metodos dentro da string
+puts "O tamnho  da minha string é : #{mensagem.length() }"
